148143fc935e534b847593fc981bb316deb7a23a
Dewey Banks <DeweyBanks@gmail.com>
2017-10-09 17:31:58 -0400

    adds auth token to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   context 'validations' do
     before { @user = FactoryGirl.build(:user) }
     it { is_expected.to validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:auth_token)}
   end
 
   context 'with valid attributes' do
@@ -19,5 +19,20 @@ RSpec.describe User, type: :model do
      before { @user = FactoryGirl.build(:user, email: "") }
      subject {@user}
      it { is_expected.to be_invalid }
+  end
+
+  describe "#generate_authentication_token!" do
+    before { @user = FactoryGirl.build(:user) }
+    it "generates a unique token" do
+      allow(Devise).to receive(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "auniquetoken123"
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
   end
 end
