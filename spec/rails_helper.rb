2a81d62eda515a27ef9aab32846842f9bce96e07
Dewey Banks <DeweyBanks@gmail.com>
2017-10-09 17:13:50 -0400

    adds request helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -68,4 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
